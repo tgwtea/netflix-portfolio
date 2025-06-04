551e1ef45ea66fc7b6f85515c8d7584c1aff80ae
guanwei <105268542+tgwtea@users.noreply.github.com>
2025-06-05 05:28:07 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Tailwind is working ✅</h1>
+    </div>
+  );
+}
+
+export default App;
